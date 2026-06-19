8dba1b0abe355fafeb064ea4db68ce60b57e7fbe
David Koelle <david2@21bbbaa8-be31-4401-adf8-cd02224cebcc.fritz.box>
2026-06-19 09:18:30 +0200

    Bypass favicon cache on homepages

---
+++ src/_data/favicon.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+function readDataUri(fileName, mimeType) {
+  const filePath = path.join(__dirname, "..", fileName);
+  const bytes = fs.readFileSync(filePath);
+  return `data:${mimeType};base64,${bytes.toString("base64")}`;
+}
+
+module.exports = {
+  png32DataUri: readDataUri("favicon-32x32-20260619.png", "image/png"),
+  png16DataUri: readDataUri("favicon-16x16-20260619.png", "image/png")
+};
